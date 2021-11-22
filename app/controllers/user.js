996424bdc772cf53794aa8064ac35f1552f65996
Chouvier Maxime <maxime.chouvier@gmail.com>
2021-11-22 19:16:56 +0100

    Authentifcation basique DONE , wip tokens

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,37 +3,52 @@ const bcrypt = require('bcrypt');
 const sequelize = require('sequelize')
 const router = express.Router();
 const models = require("../models");
+const { request } = require('express');
 
 
 exports.signup = (req, res, next) => {
-console.log(req.body)
-bcrypt
-.hash(req.body.password, 10)
-.then((hash) => {
-  const user = models.User.create({
-    email: req.body.email,
-    name: req.body.name,
-    firstname: req.body.firstname,
-    password: hash,
+models.User.findOne({where : {email: req.body.email}})
+  .then((user) => {
+    if (!user){
+      bcrypt
+      .hash(req.body.password, 10)
+      .then((hash) => {
+        const user = models.User.create({
+          email: req.body.email,
+          name: req.body.name,
+          firstname: req.body.firstname,
+          password: hash,
+          isAdmin: true
   })
     .then((user) => {
       res.status(201).json({
-        userId: user.id,
-        isAdmin: user.isAdmin,
+        userId: user.id
       });
     })
     .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
     .catch((error) => res.status(400).json({ error}));
 })
 .catch((error) => res.status(500).json({ error: error }));
+    } else {
+      res.status(401).json({error: "Un utilisateur utilise déja cet email"})
+    }
+  })
+
 };
 
 exports.login = (req, res, next) => {
-    res.send("yo");
+models.User.findOne({where : {email: req.body.email}})
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({error: "User not found"})
+      } else {
+        bcrypt.compare (req.body.password, user.password)
+          .then ((valid) => {
+            if (!valid) {
+              return res.status(401).json({ error: "User found but passwords aren't matching"})
+            }
+            res.status(200).json({message: "Vous êtes connecté"})
+          })
+      }
+    })
 };
-
-// email: DataTypes.STRING,
-// name: DataTypes.STRING,
-// firstName: DataTypes.STRING,
-// password: DataTypes.STRING,
-// isAdmin: DataTypes.BOOLEAN
