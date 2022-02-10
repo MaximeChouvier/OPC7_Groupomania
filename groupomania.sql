4782b89473a13906d20395ac9ef0e601fa603d44
Chouvier Maxime <maxime.chouvier@gmail.com>
2022-02-10 14:46:56 +0100

    Cleaned root folder, fixed SQL dump

--- groupomania.sql
+++ groupomania.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mar. 08 fév. 2022 à 03:19
+-- Généré le : jeu. 10 fév. 2022 à 13:40
 -- Version du serveur : 8.0.27
 -- Version de PHP : 7.4.19
 
@@ -78,11 +78,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `name`, `firstname`, `password`, `imgUrl`, `isAdmin`, `createdAt`, `updatedAt`) VALUES
-(47, 'admin@admin.fr', 'Chouvier', 'Maxime', '$2b$10$4sOmfX3byeTSXaJ3fR7USuXtBJ8HGJXa8lBlcCYefcqM9OGEO1e9S', 'http://localhost:3000/uploads/basic.jpg', 1, '2022-02-04 12:36:50', '2022-02-04 12:36:50'),
+(47, 'admin@admin.fr', 'Chouvier', 'Maxime', '$2b$10$4sOmfX3byeTSXaJ3fR7USuXtBJ8HGJXa8lBlcCYefcqM9OGEO1e9S', 'http://localhost:3000/uploads/1644493011765.jpeg', 1, '2022-02-04 12:36:50', '2022-02-10 11:36:51'),
 (48, 'user@user.fr', 'Dupont', 'Damien', '$2b$10$Bvi0/d.TAb9TYH5sNp1GlO66IjuTU3kL52vN7ymEwulh2JjYn5c62', 'http://localhost:3000/uploads/basic.jpg', 0, '2022-02-08 02:58:54', '2022-02-08 02:58:54'),
 (49, 'admin2@admin.fr', 'Graal', 'Naomi', '$2b$10$lxNk3E3U/5fnK586Y0WWZeQuv9zXP9bQ92cHsB/roKejvPkzpuHNa', 'http://localhost:3000/uploads/basic.jpg', 1, '2022-02-08 03:04:18', '2022-02-08 03:04:18'),
 (50, 'user2@user.fr', 'Moaji', 'Sabrina', '$2b$10$SAH/kxkUgOxnj9ZanNnNiunHbDfZhXVw.FuWKZGJ56iHMcWUUz4Sm', 'http://localhost:3000/uploads/basic.jpg', 0, '2022-02-08 03:04:56', '2022-02-08 03:04:56'),
-(51, 'user3@user.fr', 'Bisnor', 'Gabriel', '$2b$10$h9qd7MMWw47XPhQpo6cMDONyQ.0IQ8UAPVwwjqQ2g9q6HMzH/q56.', 'http://localhost:3000/uploads/basic.jpg', 0, '2022-02-08 03:05:41', '2022-02-08 03:05:41');
+(51, 'user3@user.fr', 'Bisnor', 'Gabriel', '$2b$10$h9qd7MMWw47XPhQpo6cMDONyQ.0IQ8UAPVwwjqQ2g9q6HMzH/q56.', 'http://localhost:3000/uploads/basic.jpg', 0, '2022-02-08 03:05:41', '2022-02-08 03:05:41'),
+(55, 'gabriel@mail.fr', 'Dupont', 'Enzo', '$2b$10$O0xo1F8jbdtSGE31v6eIne1zngEHgQEOlZh0ZxxJCiUbzAysE21UO', 'http://localhost:3000/uploads/1644499099631.jpeg', 0, '2022-02-10 13:06:39', '2022-02-10 13:18:19');
 
 --
 -- Index pour les tables déchargées
@@ -117,19 +118,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT pour la table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=172;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=184;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- Contraintes pour les tables déchargées
